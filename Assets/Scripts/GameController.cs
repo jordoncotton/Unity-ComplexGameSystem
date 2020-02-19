5f34970175447d016ab7d1ca17471eab57ebcead
jordoncotton1 <jordoncotton1@gmail.com>
2020-02-18 18:10:13 -0600

    push

--- Assets/Scripts/GameController.cs
+++ Assets/Scripts/GameController.cs
@@ -1,18 +1,107 @@
-﻿using System.Collections;
-using System.Collections.Generic;
+﻿using UnityEngine.UI;
 using System;
 using UnityEngine;
 
 [RequireComponent(typeof(MazeConstructor))]
 public class GameController : MonoBehaviour
 {
+    //1
+    [SerializeField] private FpsMovement player;
+    [SerializeField] private Text timeLabel;
+    [SerializeField] private Text scoreLabel;
+
     private MazeConstructor generator;
 
+    //2
+    private DateTime startTime;
+    private int timeLimit;
+    private int reduceLimitBy;
 
-	// Use this for initialization
-	void Start ()
+    private int score;
+    private bool goalReached;
+
+    //3
+    void Start()
     {
         generator = GetComponent<MazeConstructor>();
-        generator.GenerateNewMaze(13, 15);
+        StartNewGame();
+    }
+
+    //4
+    private void StartNewGame()
+    {
+        timeLimit = 80;
+        reduceLimitBy = 5;
+        startTime = DateTime.Now;
+
+        score = 0;
+        scoreLabel.text = score.ToString();
+
+        StartNewMaze();
+    }
+
+    //5
+    private void StartNewMaze()
+    {
+        generator.GenerateNewMaze(13, 15, OnStartTrigger, OnGoalTrigger);
+
+        float x = generator.startCol * generator.hallWidth;
+        float y = 1;
+        float z = generator.startRow * generator.hallWidth;
+        player.transform.position = new Vector3(x, y, z);
+
+        goalReached = false;
+        player.enabled = true;
+
+        // restart timer
+        timeLimit -= reduceLimitBy;
+        startTime = DateTime.Now;
+    }
+
+    //6
+    void Update()
+    {
+        if (!player.enabled)
+        {
+            return;
+        }
+
+        int timeUsed = (int)(DateTime.Now - startTime).TotalSeconds;
+        int timeLeft = timeLimit - timeUsed;
+
+        if (timeLeft > 0)
+        {
+            timeLabel.text = timeLeft.ToString();
+        }
+        else
+        {
+            timeLabel.text = "TIME UP";
+            player.enabled = false;
+
+            Invoke("StartNewGame", 4);
+        }
+    }
+
+    //7
+    private void OnGoalTrigger(GameObject trigger, GameObject other)
+    {
+        Debug.Log("Goal!");
+        goalReached = true;
+
+        score += 1;
+        scoreLabel.text = score.ToString();
+
+        Destroy(trigger);
+    }
+
+    private void OnStartTrigger(GameObject trigger, GameObject other)
+    {
+        if (goalReached)
+        {
+            Debug.Log("Finish!");
+            player.enabled = false;
+
+            Invoke("StartNewMaze", 4);
+        }
     }
 }
--- Assets/Scripts/MazeConstructor.cs
+++ Assets/Scripts/MazeConstructor.cs
@@ -18,7 +18,34 @@ public class MazeConstructor : MonoBehaviour
         get; private set;
     }
 
-	void Awake()
+    public float hallWidth
+    {
+        get; private set;
+    }
+    public float hallHeight
+    {
+        get; private set;
+    }
+
+    public int startRow
+    {
+        get; private set;
+    }
+    public int startCol
+    {
+        get; private set;
+    }
+
+    public int goalRow
+    {
+        get; private set;
+    }
+    public int goalCol
+    {
+        get; private set;
+    }
+
+    void Awake()
     {
         dataGenerator = new MazeDataGenerator();
         meshGenerator = new MazeMeshGenerator();
@@ -48,15 +75,108 @@ public class MazeConstructor : MonoBehaviour
         mr.materials = new Material[2] { mazeMat1, mazeMat2 };
     }
 
-    public void GenerateNewMaze(int sizeRows, int sizeCols)
+    private void PlaceStartTrigger(TriggerEventHandler callback)
+    {
+        GameObject go = GameObject.CreatePrimitive(PrimitiveType.Cube);
+        go.transform.position = new Vector3(startCol * hallWidth, .5f, startRow * hallWidth);
+        go.name = "Start Trigger";
+        go.tag = "Generated";
+
+        go.GetComponent<BoxCollider>().isTrigger = true;
+        go.GetComponent<MeshRenderer>().sharedMaterial = startMat;
+
+        TriggerEventRouter tc = go.AddComponent<TriggerEventRouter>();
+        tc.callback = callback;
+    }
+
+    private void PlaceGoalTrigger(TriggerEventHandler callback)
+    {
+        GameObject go = GameObject.CreatePrimitive(PrimitiveType.Cube);
+        go.transform.position = new Vector3(goalCol * hallWidth, .5f, goalRow * hallWidth);
+        go.name = "Treasure";
+        go.tag = "Generated";
+
+        go.GetComponent<BoxCollider>().isTrigger = true;
+        go.GetComponent<MeshRenderer>().sharedMaterial = treasureMat;
+
+        TriggerEventRouter tc = go.AddComponent<TriggerEventRouter>();
+        tc.callback = callback;
+    }
+
+    public void GenerateNewMaze(int sizeRows, int sizeCols,
+    TriggerEventHandler startCallback = null, TriggerEventHandler goalCallback = null)
     {
         if (sizeRows % 2 == 0 && sizeCols % 2 == 0)
         {
             Debug.LogError("Odd numbers work better for dungeon size.");
         }
+
+        DisposeOldMaze();
+
         data = dataGenerator.FromDimensions(sizeRows, sizeCols);
 
+        FindStartPosition();
+        FindGoalPosition();
+
+        // store values used to generate this mesh
+        hallWidth = meshGenerator.width;
+        hallHeight = meshGenerator.height;
+
         DisplayMaze();
+
+        PlaceStartTrigger(startCallback);
+        PlaceGoalTrigger(goalCallback);
+    }
+
+
+    public void DisposeOldMaze()
+    {
+        GameObject[] objects = GameObject.FindGameObjectsWithTag("Generated");
+        foreach (GameObject go in objects)
+        {
+            Destroy(go);
+        }
+    }
+
+    private void FindStartPosition()
+    {
+        int[,] maze = data;
+        int rMax = maze.GetUpperBound(0);
+        int cMax = maze.GetUpperBound(1);
+
+        for (int i = 0; i <= rMax; i++)
+        {
+            for (int j = 0; j <= cMax; j++)
+            {
+                if (maze[i, j] == 0)
+                {
+                    startRow = i;
+                    startCol = j;
+                    return;
+                }
+            }
+        }
+    }
+
+    private void FindGoalPosition()
+    {
+        int[,] maze = data;
+        int rMax = maze.GetUpperBound(0);
+        int cMax = maze.GetUpperBound(1);
+
+        // loop top to bottom, right to left
+        for (int i = rMax; i >= 0; i--)
+        {
+            for (int j = cMax; j >= 0; j--)
+            {
+                if (maze[i, j] == 0)
+                {
+                    goalRow = i;
+                    goalCol = j;
+                    return;
+                }
+            }
+        }
     }
 
     void OnGUI()
